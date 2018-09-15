9bd8443df0ef0ae9b1f6d250d3f80fd02980cad9
Jonas Gorauskas <jgorauskas@gmail.com>
2018-09-14 19:35:12 -0800

    adding main program class

---
+++ Euler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace Euler {
+    class Program {
+        private static string EULER_CLASS = "Euler.Solutions.Euler";
+
+        static void Main(string[] args) {
+
+            try {
+
+                Util.ParseOptions(args);
+
+                Assembly asm = Assembly.LoadFrom(Assembly.GetExecutingAssembly().Location);
+                var t = asm.GetType(Program.EULER_CLASS + Util.Problem, true, true);
+                var obj = (IEuler)Activator.CreateInstance(t);
+
+                if (Util.Verbose) {
+                    Util.W(obj.Problem);
+                }
+
+                Util.WL(obj.Answer);
+
+            } catch (TypeLoadException) {
+
+                if (string.IsNullOrEmpty(Util.Problem)) {
+                    Util.WL("Please provide a Project Euler problem number...");
+                    Util.WL("Enter euler.exe -? for usage information");
+                } else {
+                    Util.WL("Unable to load and execute solution to Project Euler problem " + Util.Problem);
+                }
+
+            }
+        }
+    }
+}
