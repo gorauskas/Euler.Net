ab26b2bc9010504dbb1ab25ac96558d3e7c4fa85
Jonas Gorauskas <jgorauskas@gmail.com>
2018-09-21 18:35:50 -0800

    adding problem 2 test

---
+++ EulerTest/Euler2Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler2Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler2 e = new Euler2();
+            Assert.AreEqual(4613732.0, e.Solve());
+
+        }
+
+    }
+
+}
