fc950cc662ddf25ddff287995fc192bf2cf7b1bb
Jonas Gorauskas <jgorauskas@gmail.com>
2018-09-22 23:19:22 -0800

    adding problem 3 test

---
+++ EulerTest/Euler3Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler3Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler3 e = new Euler3();
+            Assert.AreEqual(6857.0, e.Solve());
+
+        }
+
+    }
+
+}
