bd644a6ce647355947a4d649760f303a85ff099e
Jonas Gorauskas <jgorauskas@gmail.com>
2018-09-25 20:22:26 -0800

    adding problem 4 test

---
+++ EulerTest/Euler4Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler4Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler4 e = new Euler4();
+            Assert.AreEqual(906609.0, e.Solve());
+
+        }
+
+    }
+
+}
