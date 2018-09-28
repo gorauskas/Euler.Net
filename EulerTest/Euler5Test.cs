01a209acb10bc6b7545f39837e572d5e595fcf54
Jonas Gorauskas <jgorauskas@gmail.com>
2018-09-27 22:58:42 -0800

    adding problem 5 test

---
+++ EulerTest/Euler5Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler5Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler5 e = new Euler5();
+            Assert.AreEqual(232792560.0, e.Solve());
+
+        }
+
+    }
+
+}
