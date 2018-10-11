d97e2b91c2cf7a2dd648d9fd33254ab017648aa2
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-10 23:35:32 -0800

    adding problem 8 test

---
+++ EulerTest/Euler8Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    class Euler8Test {
+
+        [Test]
+        public void TestSolve() {
+            Euler8 e = new Euler8();
+            Assert.AreEqual(23514624000.0, e.Solve());
+        }
+    }
+}
