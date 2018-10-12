9458a463b78d314036f76e4f12d7b9c033f5f4db
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-11 18:05:12 -0800

    refactored problem 8 test for readability

--- EulerTest/Euler8Test.cs
+++ EulerTest/Euler8Test.cs
@@ -4,12 +4,16 @@ using Euler.Solutions;
 
 namespace Euler.Tests {
 
-    class Euler8Test {
+    public class Euler8Test {
 
         [Test]
         public void TestSolve() {
+
             Euler8 e = new Euler8();
             Assert.AreEqual(23514624000.0, e.Solve());
+
         }
+
     }
+
 }
