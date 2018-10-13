543404d40949ebb17bd0f80fb48a06fc26e5a78d
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-12 23:01:44 -0800

    adding problem 9 test

---
+++ EulerTest/Euler9Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler9Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler9 e = new Euler9();
+            Assert.AreEqual(31875000.0, e.Solve());
+
+        }
+
+    }
+
+}
