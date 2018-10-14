86ea2fdc01c845fcc3a800d73451134a40c29ab7
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-13 19:40:18 -0800

    adding problem 10 solution

---
+++ Euler/Solutions/Euler10.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Euler.Sequences;
+
+namespace Euler.Solutions {
+
+    public class Euler10 : IEuler {
+
+        public string Problem {
+            get {
+                return @"
+Project Euler Problem 10:
+
+   The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+   Find the sum of all the primes below two million.
+
+";
+            }
+        }
+
+        public string Answer {
+            get {
+                return "The sum of all primes below 2 million is: {0}".FormatWith(this.Solve());
+            }
+        }
+
+        public double Solve() {
+            return new LongPrimeSequence().AsParallel().TakeWhile(x => x < 2000000).Sum();
+        }
+
+    }
+
+}
