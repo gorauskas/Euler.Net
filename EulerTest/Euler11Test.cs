5fed66a7dcc1a2bf4eff539b08183af615a060bd
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-14 22:33:51 -0800

    adding problem 11 test

---
+++ EulerTest/Euler11Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler11Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler11 e = new Euler11();
+            Assert.AreEqual(70600674.0, e.Solve());
+
+        }
+
+    }
+
+}
