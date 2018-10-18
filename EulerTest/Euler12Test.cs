e5431df78b8b4484ef3abd993ce2a7692486f455
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-17 18:58:40 -0800

    adding problem 12 test

---
+++ EulerTest/Euler12Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler12Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler12 e = new Euler12();
+            Assert.AreEqual(76576500.0, e.Solve());
+
+        }
+
+    }
+
+}
