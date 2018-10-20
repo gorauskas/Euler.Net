3f62b5e8ccfdfa7279eec4a4f9937dddc4c33bb0
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-19 20:19:47 -0800

    adding problem 13 test

---
+++ EulerTest/Euler13Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler13Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler13 e = new Euler13();
+            Assert.AreEqual(5537376230.0, e.Solve());
+
+        }
+
+    }
+
+}
