ea631c8f320714a06ef5e40c507d5729c875ef21
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-25 21:37:35 -0800

    adding problem 15 solution

---
+++ Euler/Solutions/Euler15.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace Euler.Solutions {
+    public class Euler15 : IEuler {
+        public string Problem {
+            get {
+                return @"
+Project Euler Problem 15:
+
+    Starting in the top left corner of a 2x2 grid, there are 6 routes (without 
+    backtracking) to the bottom right corner.
+
+    How many routes are there through a 20x20 grid?
+
+";
+            }
+        }
+
+        public string Answer {
+            get {
+                return "There are {0} routes through a 20x20 grid"
+                    .FormatWith(this.Solve());
+            }
+        }
+
+        public double Solve() {
+            return (double)40.Choose(20);
+        }
+
+    }
+}
