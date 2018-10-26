ff0d7bef3fd2c9034156923705c4d29922c1781f
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-25 21:07:53 -0800

    adding problem 15 test

---
+++ EulerTest/Euler15Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler15Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler15 e = new Euler15();
+            Assert.AreEqual(137846528820.0, e.Solve());
+
+        }
+
+    }
+
+}
