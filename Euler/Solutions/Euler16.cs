d8909a0a15c6e1b52f49895bd54d3d021ef7b355
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-27 22:30:51 -0800

    adding problem 16 solution

---
+++ Euler/Solutions/Euler16.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Numerics;
+
+namespace Euler.Solutions {
+    public class Euler16 : IEuler {
+        public string Problem {
+            get {
+                return @"
+Project Euler Problem 16:
+
+    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2^1000?
+
+";
+            }
+        }
+
+        public string Answer {
+            get {
+                return "The sum of the digits of the number 2^1000 is: {0}"
+                    .FormatWith(this.Solve());
+            }
+        }
+
+        public double Solve() {
+            return BigInteger.Pow(2, 1000)
+                    .ToString()
+                    .ToByteSequence()
+                    .Sum();
+        }
+    }
+}
