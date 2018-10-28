299a9befd4408e7c6940d28025408faa5afbea84
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-27 22:03:15 -0800

    adding problem 16 test

---
+++ EulerTest/Euler16Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler16Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler16 e = new Euler16();
+            Assert.AreEqual(1366.0, e.Solve());
+
+        }
+
+    }
+
+}
