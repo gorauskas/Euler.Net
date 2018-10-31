4f14c88dedd857884c762118f6d7d6348920a71e
Jonas Gorauskas <jgorauskas@gmail.com>
2018-10-30 19:45:53 -0800

    adding problem 17 test

---
+++ EulerTest/Euler17Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler17Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler17 e = new Euler17();
+            Assert.AreEqual(21124.0, e.Solve());
+
+        }
+
+    }
+
+}
