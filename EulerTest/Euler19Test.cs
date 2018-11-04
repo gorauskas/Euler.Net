cb6bcf67f53062e458f404e26c3f9032825cc612
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-03 19:30:10 -0800

    adding problem 19 test

---
+++ EulerTest/Euler19Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler19Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler19 e = new Euler19();
+            Assert.AreEqual(171.0, e.Solve());
+
+        }
+
+    }
+
+}
