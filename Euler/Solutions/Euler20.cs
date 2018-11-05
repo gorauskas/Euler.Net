f2bdf4064716cac89c88604df0a1f33cd14e1f85
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-04 22:11:58 -0800

    adding problem 20 solution

---
+++ Euler/Solutions/Euler20.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler.Solutions {
+    public class Euler20 : IEuler {
+        public string Problem {
+            get {
+                return @"
+Project Euler Problem 20:
+
+    n! means n * (n - 1) * ... * 3 * 2 * 1
+
+    For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+    and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+    Find the sum of the digits in the number 100!
+
+";
+            }
+        }
+
+        public string Answer {
+            get {
+                return "The sum of the digits in the number 100! is: {0}"
+                    .FormatWith(this.Solve());
+            }
+        }
+
+        public double Solve() {
+            return 100.BigFactorial()
+                .ToString()
+                .Select(x => Int32.Parse(x.ToString()))
+                .Sum();
+        }
+    }
+}
