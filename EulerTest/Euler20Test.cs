86daf258cedf0f25e95cd96ddd3619c471519592
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-04 22:00:01 -0800

    adding problem 20 test

---
+++ EulerTest/Euler20Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler20Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler20 e = new Euler20();
+            Assert.AreEqual(648.0, e.Solve());
+
+        }
+
+    }
+
+}
