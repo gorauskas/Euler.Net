07ed6b422d5440ffdb51bb878e4a3ff8f4aa882e
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-06 18:12:53 -0800

    adding problem 21 test

---
+++ EulerTest/Euler21Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler21Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler21 e = new Euler21();
+            Assert.AreEqual(31626.0, e.Solve());
+
+        }
+
+    }
+
+}
