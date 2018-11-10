2c1120acc1fcb1d42f848b49c96aad985baf0ae7
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-09 21:22:11 -0800

    adding problem 22 test

---
+++ EulerTest/Euler22Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler22Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler22 e = new Euler22();
+            Assert.AreEqual(871198282.0, e.Solve());
+
+        }
+
+    }
+
+}
