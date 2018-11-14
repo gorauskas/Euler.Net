f53e98f6f132de7072f31d158a1ca7c80bb9de6f
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-13 22:12:54 -0800

    adding problem 24 test

---
+++ EulerTest/Euler24Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler24Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler24 e = new Euler24();
+            Assert.AreEqual(2783915460.0, e.Solve());
+
+        }
+
+    }
+
+}
