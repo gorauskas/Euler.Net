993d9d5ef85591f473e230308c37083723123712
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-17 22:02:06 -0800

    adding problem 27 test

---
+++ EulerTest/Euler27Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler27Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler27 e = new Euler27();
+            Assert.AreEqual(-59231.0, e.Solve());
+
+        }
+
+    }
+
+}
