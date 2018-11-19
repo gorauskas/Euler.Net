6a40c07a47c285046139de9ceef45a63a54f898b
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-18 18:36:03 -0800

    adding problem 28 test

---
+++ EulerTest/Euler28Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler28Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler28 e = new Euler28();
+            Assert.AreEqual(669171001.0, e.Solve());
+
+        }
+
+    }
+
+}
