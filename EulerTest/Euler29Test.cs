1b841cfde621c6271c0a35a427301e2bc9c1fa07
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-19 23:02:14 -0800

    adding problem 29 test

---
+++ EulerTest/Euler29Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler29Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler29 e = new Euler29();
+            Assert.AreEqual(9183.0, e.Solve());
+
+        }
+
+    }
+
+}
