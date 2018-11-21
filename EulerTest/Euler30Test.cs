0f6a59bcca9da3023e8fb7ee8be1ff63db6c5379
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-20 22:33:16 -0800

    adding problem 30 test

---
+++ EulerTest/Euler30Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler30Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler30 e = new Euler30();
+            Assert.AreEqual(443839.0, e.Solve());
+
+        }
+
+    }
+
+}
