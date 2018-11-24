c670e227517bf34b28a5d77c7aa52be7dc94e7cb
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-23 23:12:11 -0800

    adding problem 32 test

---
+++ EulerTest/Euler32Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler32Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler32 e = new Euler32();
+            Assert.AreEqual(45228.0, e.Solve());
+
+        }
+
+    }
+
+}
