460612ac547903470a301073ffa8bb24e5e2c228
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-25 19:20:07 -0800

    adding problem 34 test

---
+++ EulerTest/Euler34Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler34Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler34 e = new Euler34();
+            Assert.AreEqual(40730.0, e.Solve());
+
+        }
+
+    }
+}
