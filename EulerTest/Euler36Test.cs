5e3cc9466c610c081b37306ec935153fb11e2a4f
Jonas Gorauskas <jgorauskas@gmail.com>
2018-11-27 21:03:08 -0800

    adding problem 36 test

---
+++ EulerTest/Euler36Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler36Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler36 e = new Euler36();
+            Assert.AreEqual(872187.0, e.Solve());
+
+        }
+
+    }
+
+}
