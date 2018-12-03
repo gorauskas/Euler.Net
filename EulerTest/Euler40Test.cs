402c8da9ff63763aeef180a349ca5ccd4f19d2b5
Jonas Gorauskas <jgorauskas@gmail.com>
2018-12-02 19:12:32 -0800

    adding problem 40 test

---
+++ EulerTest/Euler40Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+    public class Euler40Test {
+
+        [Test]
+        public void TestSolve() {
+
+            Euler40 e = new Euler40();
+            Assert.AreEqual(210.0, e.Solve());
+
+        }
+
+    }
+
+}
