fb2ee366722be89e7543e7d006f0ae17ecfe9278
Jonas Gorauskas <jgorauskas@gmail.com>
2018-12-03 19:18:29 -0800

    adding problem 41 solution

---
+++ Euler/Solutions/Euler41.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Euler.Sequences;
+
+namespace Euler.Solutions {
+    public class Euler41 : IEuler {
+        public string Problem {
+            get {
+                return @"
+Project Euler Problem 41:
+
+    We shall say that an n-digit number is pandigital if it makes use of all
+    the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital
+    and is also prime.
+
+    What is the largest n-digit pandigital prime that exists?
+
+";
+            }
+        }
+
+        public string Answer {
+            get {
+                return "The largest n-digit pandigital prime that exists is {0}"
+                    .FormatWith(this.Solve());
+            }
+        }
+
+        public double Solve() {
+            long n = 7654321;
+
+            while (!(n.IsPrime() && n.IsPanDigital(7))) {
+                n -= 2;
+            }
+
+            return n;
+        }
+    }
+}
