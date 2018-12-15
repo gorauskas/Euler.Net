8eb2a10b996092b22187d1c209fdb5071c204103
Jonas Gorauskas <jgorauskas@gmail.com>
2018-12-14 19:02:48 -0800

    adding problem 44 test

---
+++ EulerTest/Euler44Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler44Test {
+
+        [Test]
+        public void TestSolve044() {
+
+            Euler44 e = new Euler44();
+            Assert.AreEqual(5482660.0, e.Solve());
+
+        }
+
+    }
+
+}
