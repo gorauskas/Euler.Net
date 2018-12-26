8bf4fb2ad6e3e691c4adf8ae4ae423c67b0cb6e0
Jonas Gorauskas <jgorauskas@gmail.com>
2018-12-25 20:25:41 -0800

    adding problem 51 test

---
+++ EulerTest/Euler51Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler51Test {
+
+        [Test]
+        public void TestSolve051() {
+
+            Euler51 e = new Euler51();
+            Assert.AreEqual(121313.0, e.Solve());
+
+        }
+
+    }
+
+}
