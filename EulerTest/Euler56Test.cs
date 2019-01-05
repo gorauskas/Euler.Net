b7c3783c1c53f97b364defe3603ba179896b1440
Jonas Gorauskas <jgorauskas@gmail.com>
2019-01-04 20:59:20 -0800

    adding problem 56 test

---
+++ EulerTest/Euler56Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Euler.Solutions;
+
+namespace Euler.Tests {
+
+    public class Euler56Test {
+
+        [Test]
+        public void TestSolve056() {
+
+            Euler56 e = new Euler56();
+            Assert.AreEqual(972.0, e.Solve());
+
+        }
+
+    }
+
+}
